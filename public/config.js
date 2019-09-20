75aaa86ea2df1c8b0aef66bc564bc4f7769a91ba
Gabriel Caiana <gabriel@server.signainfo.com.br>
2019-09-20 18:04:46 -0300

    retornando imagens da api e listando

---
+++ public/config.js
@@ -0,0 +1,3 @@
+export default {
+  api_key: '217e93e7adb4805e1781d0b68d1c7027'
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form>
+      <label>
+        Procurar:
+        <input v-model="tag" type="text">
+        <button type="submit" class="go-button" @click.prevent="search">Procurar</button>
+      </label>
+    </form>
+    <p v-if="loading">Loading ...</p>
+    <ul v-else>
+     <li v-for="image in images" :key="image.id">
+       <img :src="image.url_n" alt="image.title"/>
+       <div>
+         <p v-if="image.title"> {{ image.title }}</p>
+         <p v-else>Nenhum titulo encontrado</p>
+         <p>Por {{image.ownername}}</p>
+         <p> Visualização: {{ image.views}}</p>
+       </div>
+     </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import config from '../../public/config';
+import axios from 'axios';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      tag: '',
+      loading: false,
+      images: []
+    }
   },
-};
+  methods: {
+    search() {
+      this.loading = true
+      this.fetchImages()
+        .then((response)=> {
+          this.images = response.data.photos.photo
+          this.loading = false
+        })
+    },
+    fetchImages() {
+      return axios({
+        method: 'get',
+        url: 'https://api.flickr.com/services/rest',
+        params: {
+          method: 'flickr.photos.search',
+          api_key: config.api_key,
+          tags: this.tag,
+          extras: 'url_n, owner_name, date_taken, views',
+          page: 1,
+          format: 'json',
+          nojsoncallback: 1,
+          per_page: 30,
+        }
+      })
+    },
+  }
+}
 </script>
