5f945ebba4357a8115c3fc81bfab3ab891885b10
Gabriel Caiana <gabrielcaiana@hotmail.com>
2019-09-22 01:38:33 -0300

    complete gallery

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,64 @@
+
 <template>
-  <div class="home">
-    <form>
-      <label>
-        Procurar:
-        <input v-model="tag" type="text">
-        <button type="submit" class="go-button" @click.prevent="search">Procurar</button>
-      </label>
-    </form>
-    <p v-if="loading">Loading ...</p>
-    <ul v-else>
-     <li v-for="image in images" :key="image.id">
-       <img :src="image.url_n" alt="image.title"/>
-       <div>
-         <p v-if="image.title"> {{ image.title }}</p>
-         <p v-else>Nenhum titulo encontrado</p>
-         <p>Por {{image.ownername}}</p>
-         <p> Visualização: {{ image.views}}</p>
-       </div>
-     </li>
-    </ul>
+  <div>
+    <nav class="navbar">
+      <form class="searchbar">
+        <label>
+          <span class='screen-reader-only'>Procurar:</span>
+          <input
+            v-model="tag"
+            placeholder="Procurar fotos"
+            type="text"
+            class="searchbar-input">
+        </label>
+        <button
+          type="submit"
+          class="btn btn--green btn--go"
+          @click.prevent="search">
+            Go
+        </button>
+      </form>
+    </nav>
+   <div class="wrapper">
+      <p v-if="loading" class="text-centered">
+        Loading...
+      </p>
+      <ul v-else class="image-card-grid">
+        <image-card
+          v-for="image in images"
+          :key="image.id"
+          :image="image" />
+      </ul>
+   </div>
   </div>
 </template>
 
 <script>
 import config from '../../public/config';
 import axios from 'axios';
-
+import imageCard from '@/components/imageCard';
 export default {
   name: 'home',
+  components: {
+    imageCard
+  },
   data() {
     return {
-      tag: '',
       loading: false,
+      tag: '',
       images: []
     }
   },
   methods: {
     search() {
-      this.loading = true
+      this.loading = true;
       this.fetchImages()
-        .then((response)=> {
-          this.images = response.data.photos.photo
-          this.loading = false
+        .then((response) => {
+          this.images = response.data.photos.photo;
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.log("An error ocurred: ", error);
         })
     },
     fetchImages() {
@@ -61,5 +78,80 @@ export default {
       })
     },
   }
-}
+};
 </script>
+
+<style lang="scss">
+.screen-reader-only {
+  height: 1px;
+  width: 1px;
+  position: absolute;
+  left: -100000px;
+}
+.text-centered {
+  text-align: center;
+}
+.wrapper {
+  margin: 0 auto;
+  max-width: 800px;
+  @media only screen and (max-width: 799px) {
+    max-width: 100%;
+    margin: 0 1.5rem;
+  }
+}
+.image-card-grid {
+  list-style: none;
+  margin: .5rem 0;
+  padding: 0;
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  background: #F0F0F0;
+}
+.searchbar {
+  width: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media only screen and (max-width: 549px) {
+    width: 100%;
+    label {
+      width: 80%;
+    }
+  }
+}
+.searchbar-input {
+  padding: .5rem 1rem;
+  border-radius: 20px;
+  font-size: 1rem;
+  border: 1px solid gray;
+  min-width: 300px;
+  @media only screen and (max-width: 549px) {
+    min-width: 0;
+    width: 100%;
+  }
+}
+.btn {
+  padding: .5rem 1rem;
+  font-size: 1rem;
+  border-radius: 20px;
+  background: transparent;
+  border: none;
+}
+.btn--green {
+  background: #42b983;
+  color: white;
+  font-weight: bold;
+}
+.btn--go {
+  padding: .5rem 2rem;
+  margin-left: 1rem;
+}
+</style>
+
